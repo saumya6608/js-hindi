e15a00a922571e9dab6b830a7c58d155dd685d08
Saumya Raj <121798077+saumya6608@users.noreply.github.com>
2025-07-04 15:27:40 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+//console.log(typeof score)
+//console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 
 // "33" => 33
 // "33abc" => Nan
@@ -13,7 +13,7 @@ console.log(valueInNumber)
 
 let isloggedIn = "1"
 let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -22,5 +22,41 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+/***************************OPERATIONS************* */
+
+let value = 3;
+let negvalue = -value
+//console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)// 2 to the power 2
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " sam"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log("true")
+// console.log(+true)
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter)
+++gameCounter
+console.log(gameCounter)
