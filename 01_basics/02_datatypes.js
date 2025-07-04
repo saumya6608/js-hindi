556dc1cb7d1959d561e1049697a7cba09b58a19b
Saumya Raj <121798077+saumya6608@users.noreply.github.com>
2025-07-04 15:05:50 +0000

    conversion of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("sam")
+
+let name = "Sam"
+let age = 21
+let isloggedIn = false
+
+/*
+datatypes
+1. number => to power 53
+2. bigint 
+3. string => ""
+4. boolean => true/false
+5. null => standalone value
+6. undefined => 
+7. symbol => unique
+8. object
+*/
+
+console.log(typeof null)
+console.log(typeof undefined)
