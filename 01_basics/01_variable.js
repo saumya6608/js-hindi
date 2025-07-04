160aed7ec53f9c3864399a98a863c1350da84074
Saumya Raj <121798077+saumya6608@users.noreply.github.com>
2025-07-04 14:26:05 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "saumyaraj@google.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+
+//accountId = 2 not allowed
+
+accountEmail = "raj@google.com"
+accountpassword = "8181818181"
+accountCity = "patna"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountpassword, accountCity])
